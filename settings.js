db62a5dbd1f2eb56a61573081e6661e05e8561e0
Hasitha00 <hasithakumara5218@gmail.com>
2025-01-30 19:37:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'CTM',
+  author: 'BOT',
+  botName: "CTM WBOT",
+  botOwner: 'C.PIYUMALL', // Your name
+  ownerNumber: '94787404891', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
